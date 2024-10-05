ec2f7fdcb0344537446e98c9704c3f3c3a5606ac
yzhong1985 <yzhong1985@gmail.com>
2024-10-05 14:08:37 -0700

    project established
    The DeepGridMCLP framework

---
+++ utils.py
@@ -0,0 +1,304 @@
+import os
+import torch
+from torch.nn import DataParallel
+import json
+import numpy as np
+import torch.nn.functional as F
+import pickle
+
+
+def log_values(cost, grad_norms, epoch, batch_id, step,
+               log_likelihood, reinforce_loss, bl_loss, tb_logger, opts):
+    avg_cost = cost.mean().item()
+    grad_norms, grad_norms_clipped = grad_norms
+
+    # Log values to screen
+    print('epoch: {}, train_batch_id: {}, avg_cost: {}'.format(epoch, batch_id, avg_cost))
+
+    print('grad_norm: {}, clipped: {}'.format(grad_norms[0], grad_norms_clipped[0]))
+    #log_dir = os.path.join(
+    #    'logs',
+    #    "{}_{}".format(opts.problem, opts.n_users),
+    #    opts.run_name
+    #)
+        
+    log_dir = os.path.join(
+        'logs',
+        "{}_{}_{}_{}".format(opts.problem, opts.n_users, opts.n_facilities, opts.p),
+        opts.run_name
+    )
+    with open(os.path.join(log_dir, 'trace.txt'), 'a+') as f:
+        f.writelines('epoch: {}, train_batch_id: {}, avg_cost: {}'.format(epoch, batch_id, avg_cost)+ '\r\n')
+    # Log values to tensorboard
+    if not opts.no_tensorboard:
+        tb_logger.log_value('avg_cost', avg_cost, step)
+
+        tb_logger.log_value('actor_loss', reinforce_loss.item(), step)
+        tb_logger.log_value('nll', -log_likelihood.mean().item(), step)
+
+        tb_logger.log_value('grad_norm', grad_norms[0], step)
+        tb_logger.log_value('grad_norm_clipped', grad_norms_clipped[0], step)
+
+        if opts.baseline == 'critic':
+            tb_logger.log_value('critic_loss', bl_loss.item(), step)
+            tb_logger.log_value('critic_grad_norm', grad_norms[1], step)
+            tb_logger.log_value('critic_grad_norm_clipped', grad_norms_clipped[1], step)
+
+
+def load_problem(name):
+    # from problems import PM, PC, MCLP
+    from problems import MCLP
+    problem = {
+        # 'PM': PM,
+        # 'PC': PC,
+        'MCLP': MCLP,
+    }.get(name, None)
+    assert problem is not None, "Currently unsupported problem: {}!".format(name)
+    return problem
+
+
+def torch_load_cpu(load_path):
+    return torch.load(load_path, map_location=lambda storage, loc: storage)  # Load on CPU
+
+
+def get_inner_model(model):
+    return model.module if isinstance(model, DataParallel) else model
+
+
+def move_to(var, device):
+    if isinstance(var, dict):
+        return {k: move_to(v, device) for k, v in var.items()}
+    return var.to(device)
+
+
+def _load_model_file(load_path, model):
+    """Loads the model with parameters from the file and returns optimizer state dict if it is in the file"""
+
+    # Load the model parameters from a saved state
+    load_optimizer_state_dict = None
+    print('  [*] Loading model from {}'.format(load_path))
+
+    load_data = torch.load(
+        os.path.join(
+            os.getcwd(),
+            load_path
+        ), map_location=lambda storage, loc: storage)
+
+    if isinstance(load_data, dict):
+        load_optimizer_state_dict = load_data.get('optimizer', None)
+        load_model_state_dict = load_data.get('model', load_data)
+    else:
+        load_model_state_dict = load_data.state_dict()
+
+    state_dict = model.state_dict()
+
+    state_dict.update(load_model_state_dict)
+
+    model.load_state_dict(state_dict)
+
+    return model, load_optimizer_state_dict
+
+
+def load_args(filename):
+    with open(filename, 'r') as f:
+        args = json.load(f)
+
+    # Backwards compatibility
+    if 'data_distribution' not in args:
+        args['data_distribution'] = None
+        probl, *dist = args['problem'].split("_")
+        if probl == "op":
+            args['problem'] = probl
+            args['data_distribution'] = dist[0]
+    return args
+
+
+def load_model(path, epoch=None):
+    from nets.attention_model import AttentionModel
+    # from nets.pointer_network import PointerNetwork
+
+    if os.path.isfile(path):
+        model_filename = path
+        path = os.path.dirname(model_filename)
+    elif os.path.isdir(path):
+        if epoch is None:
+            epoch = max(
+                int(os.path.splitext(filename)[0].split("-")[1])
+                for filename in os.listdir(path)
+                if os.path.splitext(filename)[1] == '.pt'
+            )
+        model_filename = os.path.join(path, 'epoch-{}.pt'.format(epoch))
+    else:
+        assert False, "{} is not a valid directory or file".format(path)
+
+    args = load_args(os.path.join(path, 'args.json'))
+
+    problem = load_problem(args['problem'])
+
+    model_class = {
+        'attention': AttentionModel,
+        # 'pointer': PointerNetwork
+    }.get(args.get('model', 'attention'), None)
+    assert model_class is not None, "Unknown model: {}".format(model_class)
+
+    model = model_class(
+        128,
+        128,
+        problem,
+        n_encode_layers=3,
+        mask_inner=True,
+        mask_logits=True,
+        normalization=args['normalization'],
+        tanh_clipping=args['tanh_clipping'],
+        checkpoint_encoder=args.get('checkpoint_encoder', False),
+        shrink_size=args.get('shrink_size', None)
+    )
+    # Overwrite model parameters by parameters to load
+    load_data = torch_load_cpu(model_filename)
+    model.load_state_dict({**model.state_dict(), **load_data.get('model', {})})
+
+    model, *_ = _load_model_file(model_filename, model)
+
+    model.eval()  # Put in eval mode
+
+    return model, args
+
+
+def parse_softmax_temperature(raw_temp):
+    # Load from file
+    if os.path.isfile(raw_temp):
+        return np.loadtxt(raw_temp)[-1, 0]
+    return float(raw_temp)
+
+
+def do_batch_rep(v, n):
+    if isinstance(v, dict):
+        return {k: do_batch_rep(v_, n) for k, v_ in v.items()}
+    elif isinstance(v, list):
+        return [do_batch_rep(v_, n) for v_ in v]
+    elif isinstance(v, tuple):
+        return tuple(do_batch_rep(v_, n) for v_ in v)
+
+    return v[None, ...].expand(n, *v.size()).contiguous().view(-1, *v.size()[1:])
+
+
+def sample_many(inner_func, get_cost_func, input, batch_rep=1, iter_rep=1):
+    """
+    :param input: (batch_size, graph_size, node_dim) input node features
+    :return:
+    """
+    input = do_batch_rep(input, batch_rep)
+
+    costs = []
+    pis = []
+    for i in range(iter_rep):
+        _log_p, pi = inner_func(input)
+        # pi.view(-1, batch_rep, pi.size(-1))
+        cost = get_cost_func(input, pi)
+
+        costs.append(cost.view(batch_rep, -1).t())
+        pis.append(pi.view(batch_rep, -1, pi.size(-1)).transpose(0, 1))
+
+    max_length = max(pi.size(-1) for pi in pis)
+    # (batch_size * batch_rep, iter_rep, max_length) => (batch_size, batch_rep * iter_rep, max_length)
+    pis = torch.cat(
+        [F.pad(pi, (0, max_length - pi.size(-1))) for pi in pis],
+        1
+    )  # .view(embeddings.size(0), batch_rep * iter_rep, max_length)
+    costs = torch.cat(costs, 1)
+
+    # (batch_size)
+    mincosts, argmincosts = costs.min(-1)
+    # (batch_size, minlength)
+    minpis = pis[torch.arange(pis.size(0), out=argmincosts.new()), argmincosts]
+
+    return minpis, mincosts
+
+
+def check_extension(filename):
+    if os.path.splitext(filename)[1] != ".pkl":
+        return filename + ".pkl"
+    return filename
+
+
+def save_dataset(dataset, filename):
+
+    filedir = os.path.split(filename)[0]
+
+    if not os.path.isdir(filedir):
+        os.makedirs(filedir)
+
+    with open(check_extension(filename), 'wb') as f:
+        pickle.dump(dataset, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_dataset(filename):
+
+    with open(check_extension(filename), 'rb') as f:
+        return pickle.load(f)
+    
+
+def _pad_mask(mask):
+    # By taking -size % 8, we get 0 if exactly divisible by 8
+    # and required padding otherwise (i.e. -1 % 8 = 7 pad)
+    pad = -mask.size(-1) % 8
+    if pad != 0:
+        mask = F.pad(mask, [0, pad])
+    return mask, mask.size(-1) // 8
+
+
+def _mask_bool2byte(mask):
+    assert mask.dtype == torch.uint8
+    # assert (mask <= 1).all()  # Precondition, disabled for efficiency
+    mask, d = _pad_mask(mask)
+    return (mask.view(*mask.size()[:-1], d, 8) << torch.arange(8, out=mask.new())).sum(-1, dtype=torch.uint8)
+
+
+def _mask_byte2long(mask):
+    assert mask.dtype == torch.uint8
+    mask, d = _pad_mask(mask)
+    # Note this corresponds to a temporary factor 8
+    # memory overhead by converting to long before summing
+    # Alternatively, aggregate using for loop
+    return (mask.view(*mask.size()[:-1], d, 8).long() << (torch.arange(8, dtype=torch.int64, device=mask.device) * 8)).sum(-1)
+
+
+def mask_bool2long(mask):
+    assert mask.dtype == torch.uint8
+    return _mask_byte2long(_mask_bool2byte(mask))
+
+
+def _mask_long2byte(mask, n=None):
+    if n is None:
+        n = 8 * mask.size(-1)
+    return (mask[..., None] >> (torch.arange(8, out=mask.new()) * 8))[..., :n].to(torch.uint8).view(*mask.size()[:-1], -1)[..., :n]
+
+
+def _mask_byte2bool(mask, n=None):
+    if n is None:
+        n = 8 * mask.size(-1)
+    return (mask[..., None] & (mask.new_ones(8) << torch.arange(8, out=mask.new()) * 1)).view(*mask.size()[:-1], -1)[..., :n] > 0
+
+
+def mask_long2bool(mask, n=None):
+    assert mask.dtype == torch.int64
+    return _mask_byte2bool(_mask_long2byte(mask), n=n)
+
+
+def mask_long_scatter(mask, values, check_unset=True):
+    """
+    Sets values in mask in dimension -1 with arbitrary batch dimensions
+    If values contains -1, nothing is set
+    Note: does not work for setting multiple values at once (like normal scatter)
+    """
+    assert mask.size()[:-1] == values.size()
+    rng = torch.arange(mask.size(-1), out=mask.new())
+    values_ = values[..., None]  # Need to broadcast up do mask dim
+    # This indicates in which value of the mask a bit should be set
+    where = (values_ >= (rng * 64)) & (values_ < ((rng + 1) * 64))
+    # Optional: check that bit is not already set
+    assert not (check_unset and ((mask & (where.long() << (values_ % 64))) > 0).any())
+    # Set bit by shifting a 1 to the correct position
+    # (% not strictly necessary as bitshift is cyclic)
+    # since where is 0 if no value needs to be set, the bitshift has no effect
+    return mask | (where.long() << (values_ % 64))
